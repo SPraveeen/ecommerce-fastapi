b16ecbb2f5207f656ff1c6359d6440a7e1627d4e
SPraveeen <praveenji8498@gmail.com>
2025-06-15 16:09:19 +0530

    alembic added

--- database.py
+++ database.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from .config import settings
+from config import settings
 # syntax for db connection
 # SQLALCHEMY_DATABASE_URL='postgresql://<username>:<password>@<ip-address/hostname>/<database-name>'
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
